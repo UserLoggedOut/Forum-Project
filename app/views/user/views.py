88dff07c4ec51d624ba5a708dce4b08cbb02f70d
吴超 <15835767493@163.com>
2019-06-17 17:50:43 +0800

    add: 添加了登录视图

--- app/views/user/views.py
+++ app/views/user/views.py
@@ -3,8 +3,14 @@ from flask import render_template
 from app.views.user import user_blu
 
 
+# 注册试图
 @user_blu.route("/reg")
 def reg():
     return render_template("user/reg.html")
 
+
+# 登录
+@user_blu.route("/login")
+def login():
+    return render_template("user/login.html")
 
