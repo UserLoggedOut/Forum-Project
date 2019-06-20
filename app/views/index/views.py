dec2890b261cdf00f3591449b6c23014b6989cd0
kongguanghua <13030119817@163.com>
2019-06-20 14:56:27 +0800

    index.html 和视图函数

--- app/views/index/views.py
+++ app/views/index/views.py
@@ -1,12 +1,13 @@
-from flask import render_template, request, session
+from flask import render_template, session
 
 from app import db
-from app.models.models import Detail, User, Comment
+from app.models.models import Detail, User
 from . import index_blu
 
 
 @index_blu.route("/")
 @index_blu.route("/index")
+# @login_user_data
 def index():
     # 数据库查询出来
 
@@ -14,11 +15,24 @@ def index():
 
     # 2 取出用户登录成功存储的session数据
     user_info = session.get("user_id")
+    # user_info = g.user.id
     print(user_info, "User中登录的数据")
+
+    # 3 分页
+    page = request.args.get("page", 1)
+    # 数据库查询
+    paginate = db.session.query(Detail).paginate(int(page), 2, False)
+    print(paginate)
+
+    # 4 user数据
+    details = [x.dict_detail() for x in paginate.items]
+    print("存储用户里面的所有列表打印", details)
+
     # 3 再次数据库查询User整个数据
     user = db.session.query(User).filter(User.id == user_info).first()
-    user_detail = db.session.query(Detail).order_by(Detail.clicks.desc()).limit(8)    # 主页面
-    return render_template("index.html", user=user, user_detail=user_detail)
+    user_detail = db.session.query(Detail).order_by(Detail.clicks.desc()).limit(10)    # 主页面
+    return render_template("index.html", user=user, user_detail=user_detail,
+                           paginate=paginate, details=details)
 
 
 @index_blu.route("/detail/<int:new_id>")
